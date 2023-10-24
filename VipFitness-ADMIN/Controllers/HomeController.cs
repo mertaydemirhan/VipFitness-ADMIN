d703c6e86d55a23024cf1745867a817ee3c1cbab
mertaydemirhan <59294292+mertaydemirhan@users.noreply.github.com>
2023-10-24 13:11:42 +0300

    Intermediate developments - Navbar etc.

--- VipFitness-ADMIN/Controllers/HomeController.cs
+++ VipFitness-ADMIN/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using VipFitness_ADMIN.Repositories;
 
@@ -6,8 +8,13 @@ namespace VipFitness_ADMIN.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IAntiforgery _antiforgery;
 
-        
+        public HomeController(IAntiforgery antiforgery)
+        {
+            _antiforgery = antiforgery;
+        }
+
         public ActionResult Home()
         {
 
@@ -25,7 +32,22 @@ namespace VipFitness_ADMIN.Controllers
             return RedirectToAction("Trainings", "Trainings");
         }
 
+        public ActionResult SignOut()
+        {
+            HttpContext.SignOutAsync();
 
+            // Çerezleri temizle
+            Response.Cookies.Delete("VipFitness");
+
+            // Antiforgery token'ı yeniden oluştur (CSRF saldırılarına karşı koruma sağlar)
+            ViewData["AntiForgeryToken"] = _antiforgery.GetAndStoreTokens(HttpContext).RequestToken;
+
+            // Çıkış başarılı mesajını göster
+            ViewBag.Message = "Başarıyla Çıkış Yaptınız.";
+
+            // Giriş sayfasına yönlendir
+            return View("../Login/Login");
+        }
 
     }
 }
--- VipFitness-ADMIN/Program.cs
+++ VipFitness-ADMIN/Program.cs
@@ -8,6 +8,14 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ITrainingRepository, TrainingRepository>();
 builder.Services.AddScoped<ITrainingCatRepository, TrainingCatRepository>();
 
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+
+    // Çerez adýný burada belirleyebilirsiniz
+    options.ConsentCookie.Name = "VipFitness";
+});
 
 var app = builder.Build();
 
